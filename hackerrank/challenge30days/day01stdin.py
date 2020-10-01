4cc25eb612faa08d4e548842aabd690ec7e44994
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-10-01 19:30:43 +0200

    hackerrank  problems

---
+++ hackerrank/challenge30days/day01stdin.py
@@ -0,0 +1,16 @@
+import sys
+
+def read_from_stdin_print():
+
+    input_line = sys.stdin.readline()
+    print("Hello, World.")
+    print(input_line)
+
+    # input_string = input()
+    #
+    # # Print a string literal saying "Hello, World." to stdout.
+    # print('Hello, World.')
+    #
+    # # TODO: Write a line of code here that prints the contents of input_string to stdout.
+    # print(input_string)
+read_from_stdin_print()
